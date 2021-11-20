a10fedf37af8085c70e6c6e8270e96b4e1a62304
Scytlee <Scytlee@disroot.org>
2021-11-20 13:20:44 +0100

    Added 'przegrani' command

---
+++ src/Pyrewatcher/Commands/PrzegraniCommand.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Pyrewatcher.DataAccess.Interfaces;
+using TwitchLib.Client;
+using TwitchLib.Client.Models;
+
+namespace Pyrewatcher.Commands
+{
+  [UsedImplicitly]
+  public class PrzegraniCommand : ICommand
+  {
+    private readonly TwitchClient _client;
+    private readonly IAdoZRepository _adozRepository;
+
+    public PrzegraniCommand(TwitchClient client, IAdoZRepository adozRepository)
+    {
+      _client = client;
+      _adozRepository = adozRepository;
+    }
+
+    public async Task<bool> ExecuteAsync(List<string> argsList, ChatMessage message)
+    {
+      var entries = (await _adozRepository.GetAllEntriesAsync()).ToList();
+
+      var championsWithLosses = entries
+                                  .Where(x => !x.GameWon)
+                                  .Select(x => x.ChampionName)
+                                  .OrderBy(x => x)
+                                  .ToList();
+
+      _client.SendMessage(message.Channel, string.Format(Globals.Locale["przegrani_response"], championsWithLosses));
+
+      return true;
+    }
+  }
+}
