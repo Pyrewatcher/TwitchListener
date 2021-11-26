e8b4ce01b0f2b07e8c124ca4a1eb07e6a809d894
Scytlee <Scytlee@disroot.org>
2021-11-26 17:21:50 +0100

    Added 'neon' command

---
+++ src/Pyrewatcher/Commands/NeonCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using TwitchLib.Client;
+using TwitchLib.Client.Models;
+
+namespace Pyrewatcher.Commands
+{
+    [UsedImplicitly]
+    public class NeonCommand : ICommand
+    {
+        private readonly TwitchClient _client;
+
+        public NeonCommand(TwitchClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<bool> ExecuteAsync(List<string> argsList, ChatMessage message)
+        {
+            var days = (DateTime.Now - new DateTime(2021, 10, 16)).Days;
+
+            _client.SendMessage(message.Channel, string.Format(Globals.Locale["neon_response"], days));
+
+            return await Task.FromResult(true);
+        }
+    }
+}
